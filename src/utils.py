59669cef53d0878f270a867f26809b55cffe6cd3
Omkar Mali <omkarmali235@gmail.com>
2024-06-18 23:00:36 +0530

    commiting pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 from src.logger import logging
 from src.exception import CustomException
 
@@ -21,15 +21,22 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
 
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(param.keys())[i]]
 
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)  #train the model
+            
+            
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
@@ -41,5 +48,13 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e,sys)
